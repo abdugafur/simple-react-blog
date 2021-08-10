a07403101d3fe984dbe73a111b80f5483f9680c4
abdugafur <abdugafur3323@gmail.com>
2021-08-10 11:55:16 +0500

    Finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from './conponents/Navbar';
+import Home from './Pages/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Pages/Create';
+import BlogDetails from './conponents/BlogDetails';
+import NotFound from './conponents/NotFound';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import BlogList from "../conponents/BlogList";
+import useFetch from "../hooks/useFetch";
+
+const Home = () => {
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
+
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
+  );
+}
+ 
+export default Home;
